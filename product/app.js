514c2f6b3518a605d9bdc991ac3a84adf8eff58e
Shielamaye <143503450+Shielamaye@users.noreply.github.com>
2024-02-11 13:28:55 +0800

    Add files via upload

---
+++ product/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const data = require('./data.json');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/api/products', (req, res) => {
+  res.json(data);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
